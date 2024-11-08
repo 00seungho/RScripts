6fba879f8969bdaa3d0dbf383a2c456e3b5d519e
00seungho <tmdgh9910@naver.com>
2024-11-08 11:39:41 +0900

    지도 미세먼지 분포도(전체 경도와 위도값이 있는 데이터셋) ;

--- 1101.R
+++ 1101.R
@@ -1,3 +1,9 @@
+
+install.packages("XML")
+install.packages("httr")
+install.packages("xml2")
+
+
 library(rvest)
 
 url = "https://www.epeople.go.kr/nep/prpsl/opnPrpl/opnpblPrpslList.npaid"
@@ -36,9 +42,6 @@ url= paste0(api,"?serviceKey=",apikey,"&numOfRows=",numOfRows,"&pageNo=",pageNo,
             "&searchCondition=",searchCondition)
 url
 
-install.packages("XML")
-install.packages("httr")
-install.packages("xml2")
 
 library(XML)
 library(httr)
@@ -77,13 +80,106 @@ pm = df[1, c(1:16, 19)]
 pm
 
 # 지역별 미세먼지 데이터프레임의 행과 열을 바꾸기
-pm.regin = t(pm)
-pm.regin
+df.region = t(pm)
+df.region
 
 #행렬 데이터를 데이터프레임으로 변환
-df.regin = as.data.frame(pm.regin)
-df.regin
+df.region = as.data.frame(df.region)
+df.region
 
-colnames(df.regin) = "PM10"
+colnames(df.region) = "PM10"
 
-df.regin
+df.region
+
+# df.region에 컬럼 추가(컬럼명 : name, 값 : 한글지명)
+df.region$NAME = c("대구광역시", "충청남도", "인천광역시", "대전광역시", "경상북도", "세종특별자치시", "광주광역시",
+                   "전라북도", "강원도", "울산광역시", "전라남도", "서울특별시", "부산광역시", "제주특별자치시도", "충청북도",
+                   "경상남도", "경기도")
+
+df.region
+
+
+# 행정경계 데이터셋을 사용한 지도 시각화
+library(sf)
+library(dplyr)
+
+#행정경계 데이터셋(shape[.shp] 파일) 읽어오기
+map = st_read("D:/R_data/행정경계데이터셋/Z_NGII_N3A_G0010000.shp", options = "ENCODING=EUC-KR")
+map
+ 
+ 
+#WGS84 좌표계
+crs_wgs84 = st_crs(4326)
+ 
+ 
+# WGS84 좌표게로 map을 변환
+map_sf = st_transform(map, crs_wgs84)
+ 
+ 
+# sf객체를 데이터프레임으로 변환
+df_map = st_as_sf(map_sf)
+df_map
+ 
+ 
+# 지형 정보 확인
+st_geometry((map))
+ 
+ 
+# df_map 데이터프레임의 구조 확인
+str(df_map)
+ 
+ 
+# 그룹화 변수 확인
+table(df_map$id)
+names(str(df_map))
+ 
+ 
+# 경도와 위도를 추출해서 저장
+# 현재 df_map이 유효한 데이터임을 증명
+df_map = st_make_valid(df_map)
+df_map
+ 
+# id(UFID)를 기준으로 그룹화 
+longlat = df_map %>% group_by(UFID) %>%
+                 summarise(geometry = st_centroid(st_union(geometry)), .groups="drop") %>%
+                  mutate(long=st_coordinates(geometry)[, 1], 
+                         lat=st_coordinates(geometry)[, 2]
+                         )
+longlat_df = longlat %>% st_drop_geometry() %>% as.data.frame()
+
+longlat_df
+
+str(longlat_df)
+longlat_df
+df.region
+
+# 공간정보 데이터프레임에서 기하학적 정보를 제거하는 기능
+df_map_info = st_drop_geometry(df_map)
+df_map_info
+
+longlat = cbind(longlat, NAME=df_map_info[, 3])
+longlat
+
+
+df.PM10 = merge(x=df.region, y=longlat, by="NAME", all=TRUE)
+df.PM10
+
+
+library(ggplot2)
+
+ggplot() + 
+  #  geom_sf(data=map,
+  #          fill="white",
+  #          alpha=0.5,
+  #          color="black") + 
+  geom_polygon(data=total_longlat,
+               aes(x=long, y=lat),
+               fill="white",
+               alpha=0.5,
+               color="black") + 
+  geom_point(data = df.PM10, aes(x=long, y=lat, size=PM10),
+             shape=21, color="black", fill="red", alpha=0.3) + 
+  theme(legend.position = "none") + 
+  labs(title="PM10 농동 분포", x="경도", y = "위도")
+
+
